a0bf5e81a862c3a86d9190fdb507e50289239afe
Irina <www.mokrushina@gmail.com>
2022-10-30 13:27:34 +0400

    решила задачу 27

---
+++ GIK 3/Задача 27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и
+//выдает сумму цифр в числе.
+
+int Sum (int n)
+{
+    int res = 0;
+    while (n > 0)
+    {
+        res = res + (n % 10);
+        n = n / 10;
+    }
+    return res;
+}
+Console. Clear();
+Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {N} равна {Sum (N)}");

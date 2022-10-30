f608385c20c849ae103ad699673c0998c912e8ec
Irina <www.mokrushina@gmail.com>
2022-10-30 13:14:00 +0400

    Решила задачу 25

---
+++ GIK 3/задача 25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В)
+//и возводит число А в натуральную степень В.
+
+int GetPow(int A, int B)
+{
+    int pow = 1;
+    int i = 0;
+   
+    while (i < B)
+    {
+        pow = pow * A;
+        i = i + 1;
+    }
+    return pow;
+}
+Console.Clear();
+Console.Write("Введите первое число:");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число:");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetPow(a, b));
+
